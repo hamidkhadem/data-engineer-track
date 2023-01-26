020cde655bcef37d0cf77d7dfc64edcf86919b78
Omar AlSaghier <oalsaghier@gmail.com>
2023-01-27 00:37:58 +0300

    adding data files

---
+++ Challenges/Payroll_Management_System/data_files/DB_questions.sql
@@ -0,0 +1,38 @@
+--**This file contains the problems you need to answer by writing and running the required queries:**
+
+
+-- 1) Find the Number of Employees with different degrees
+
+
+-- 2) Find the total number of employees per Department
+
+
+-- 3) Find all employees whose hourly_pay is $30. Sort them descendingly.
+
+
+-- 4) Find the hourly_payment per employee. Sort them descendingly.
+
+
+-- 5) List the first employee's first name and last name in one column in each department.
+-- limit your answer to 5 answers.
+-- Hint:
+--|-------------------------------------------------|-
+--|id	|employee_name		|dept_name				|
+--|-----|-------------------|-----------------------|-
+--|105	|Anugraha Varkey	|Business Intelligence	|
+--|-------------------------------------------------|-
+
+
+-- 6) Find the total number of employees per work_location state.
+
+-- 7) Find the total number of employees per work_location state in department "Quality Control"
+
+
+-- 8) Find the total number of employees per work_location state per each department. order them
+-- by state ascendingly, and by the sum descendingly.
+
+
+-- 9) Find How many work_location(s) have more than 4 employees
+
+
+-- 10) Find the attendance worked hours per employee's first name and last name
